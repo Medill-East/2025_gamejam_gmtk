4c80768437357d3b017fbf7ea00428dd1f5c10a9
haodong.liao <haodong.liao@gmail.com>
2025-08-03 16:09:58 +0800

    fix life label
    2025-0803-1609

--- 20250706-dodge2d/loop/scripts/loop_main.gd
+++ 20250706-dodge2d/loop/scripts/loop_main.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-	_popup()
+	#_popup()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,8 +12,8 @@ func _process(delta: float) -> void:
 	pass
 
 
-
-func _popup():
-	var popup := preload("res://loop/popup.tscn").instantiate()
-	add_child(popup)
-	popup.open()        # 打开&暂停
+#
+#func _popup():
+	##var popup := preload("res://loop/popup.tscn").instantiate()
+	#add_child(popup)
+	#popup.open()        # 打开&暂停
